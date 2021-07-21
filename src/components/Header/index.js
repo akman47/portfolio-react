c0e98b66a6ae8e754c55ddca529f216491879060
Angela Man <angela.k.man@gmail.com>
2021-07-21 12:48:56 -0500

    header and nav componenets created

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Nav';
+
+function Header() {
+
+    return(
+        <header className="flex-row">
+            <h2>Angela Man</h2>
+            <Nav></Nav>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <ul className="flex-row">
+            <li className="nav-list">
+                <a data-testid="about" href="#about">About Me</a>
+            </li>
+            <li className="nav-list">
+                <span>Portfolio</span>
+            </li>
+            <li className="nav-list">
+                <span>Contact</span>
+            </li>
+            <li className="nav-list">
+                <span>Resume</span>
+            </li>
+        </ul>
+    );
+}
+
+export default Nav;
